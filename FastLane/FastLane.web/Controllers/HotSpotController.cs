4483b609746b13deaa25839377a7f1c864f46997
juan <jlfloresjr@gmail.com>
2017-11-28 03:04:06 -0800

    submitting all

---
+++ FastLane/FastLane.web/Controllers/HotSpotController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FastLane.web.Controllers
+{
+    [RoutePrefix("hotspot")]
+    public class HotspotController : Controller
+    {
+        // GET: HotSpot
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
