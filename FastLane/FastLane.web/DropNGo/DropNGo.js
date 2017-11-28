7616363e8ceea54c55d2b653238e875180aad5f8
Natcha Kaovorakarn <natchapearl@gmail.com>
2017-11-28 08:47:39 -0800

    added stateProvider for DropNGo

--- FastLane/FastLane.web/DropNGo/DropNGo.js
+++ FastLane/FastLane.web/DropNGo/DropNGo.js
@@ -1,1 +1,41 @@
-﻿
+﻿(function () {
+    'use strict'
+
+    var app = angular.module('fastLaneApp');
+
+    app.config(configure);
+
+    configure.$inject = ['stateProvider'];
+
+    function configure($stateProider) {
+        $stateProider.state({
+            name: 'dropNGo',
+            component: 'dropNGo',
+            url: '/dropngo'
+        });
+    }
+
+    app.component('dropNGo', {
+        templateUrl: 'DropNGo/DropNGo.html',
+        controller: 'dropNGoController as vm'
+    })
+
+});
+
+(function () {
+    'use strict';
+    angular.module('fastLaneApp')
+        .controller('dropNGoController', dropNGoController);
+
+    dropNGoController.$inject = ['$state'];
+
+    function dropNGoController($state)
+    {
+        var vm = this;
+        vm.dropNGo = _dropNGo;
+    }
+
+    function _dropNGo() {
+        $state.go('dropNGo');
+    }
+});
