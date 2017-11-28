b97aa82a2c3deb065f50a4b989244d51adab3e5d
kinarki <nickfriend81@gmail.com>
2017-11-27 19:23:52 -0800

    pushing for pull from master

---
+++ FastLane/FastLane.web/Pedestrian_Safety/pedestrians.controller.js
@@ -0,0 +1,36 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module('fastLaneApp')
+        .controller('pedestrianController', PedestrianController);
+
+    PedestrianController.$inject = ['$scope', 'pedestrianService'];
+
+    function PedestrianController($scope, PedestrianService) {
+        var vm = this;
+        vm.$scope = $scope;
+        vm.$onInit = _onInit;
+        vm.pedestrianService = PedestrianService;
+        vm.auth = _auth;
+        vm.authSuccess = _authSuccess;
+        vm.authError = _authError;
+
+        function _onInit() {
+            console.log("Pedestrians say hello");
+            vm.auth();
+        }
+
+        function _auth() {
+            vm.pedestrianService.authorize()
+                .then(vm.authSuccess).catch(vm.authError);
+        }
+
+        function _authSuccess(res) {
+            console.log("Auth success: " + res);
+        }
+
+        function _authError(err) {
+            console.log("Auth error: " + err);
+        }
+    }
+})();
