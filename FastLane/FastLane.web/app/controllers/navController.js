f306573e93f8cd5fb732a35450e9efe13f4765db
ahpmu925 <ahpmu925@gmail.com>
2017-11-27 22:39:24 -0800

    dgdg

---
+++ FastLane/FastLane.web/app/controllers/navController.js
@@ -0,0 +1,37 @@
+﻿(function () {
+
+
+    angular.module('railApp')
+        .controller('navController', NavController);
+
+    NavController.$inject = ["$window", 'railService', 'toastr'];
+
+    function NavController($window, railService, toastr) {
+
+
+        var vm = this;
+
+        vm.stations = {};
+
+        vm.$onInit = _onInit;
+
+        function _onInit() {
+
+            railService.getStations(_stationSuccess, _stationError);
+
+        }
+
+
+        function _stationSuccess(response) {
+        
+            vm.stations = response.data.items;
+
+
+        };
+
+        function _stationError(response) {
+         
+        };
+
+    }
+})();
---
+++ FastLane/FastLane.web/app/services/railService.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    angular.module('railApp')
+        .factory('railService', railService);
+
+    railService.$inject = ["$log", "$http"];
+
+    function railService($log, $http) {
+
+
+        var svc = {};
+        svc.getStations = _getStations;
+        return svc;
+
+        function _getStations(onSuccess, onError) {
+
+            var settings = {
+                url: 'http://api.metro.net/agencies/lametro-rail/routes/801/stops',
+                method: 'GET'
+            };
+
+            return $http(settings)
+                .then(onSuccess, onError)
+
+        }
+        function onSuccess(Response) {
+            console.log(Response)
+        }
+
+
+    }
+})();
+
+
+
