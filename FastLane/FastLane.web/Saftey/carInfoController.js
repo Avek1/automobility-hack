886deaf0e5f5984af357f99dce80bb78c507ce12
Daniel Castro <dcastro92@hotmail.com>
2017-11-28 00:16:27 -0800

    separate transportation files

---
+++ FastLane/FastLane.web/Saftey/carInfoController.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    angular.module('fastLane')
+        .controller('carInfoController', carInfoController)
+
+    carInfoController.$inject = ['getCarService']
+
+    function carInfoController(getCarService) {
+        var vm = this;
+        vm.carInfoArray = []
+        //vm.$onInit = onInit
+        vm.tireBtn = _tireBtn
+
+        function _tireBtn() {
+            getCarService.getTireInfo().then(display)
+        }
+
+        //function onInit() {
+        //    getCarService.getTireInfo().then(display)
+        //}
+
+        function display(response) {
+            console.log(response)
+            vm.carInfoArray = response
+            console.log(vm.carInfoArray.tires[0].location)
+        }
+    }
+})();
---
+++ FastLane/FastLane.web/Saftey/getCarService.js
@@ -0,0 +1,43 @@
+﻿(function () {
+    angular.module('fastLane')
+        .service('getCarService', getCarService)
+    getCarService.$inject = ['$http', '$q']
+
+    function getCarService($http, $q) {
+        this.getTireInfo = function () {
+            var tires = $http({
+                method: 'GET',
+                url: 'https://api.smartcar.com/v1.0/vehicles/3563ad5c-d92b-4152-ac57-c720bdbd9fb6/tires',
+                headers: {
+                    Authorization: "bearer a3a1416d-53bc-4025-b760-e74129d35310"
+                }
+            }).then(resp => resp.data.tires);
+
+            var fuel = $http({
+                method: 'GET',
+                url: 'https://api.smartcar.com/v1.0/vehicles/3563ad5c-d92b-4152-ac57-c720bdbd9fb6/fuel',
+                headers: {
+                    Authorization: "bearer a3a1416d-53bc-4025-b760-e74129d35310"
+                }
+            }).then(resp => resp.data);
+
+            var location = $http({
+                method: 'GET',
+                url: 'https://api.smartcar.com/v1.0/vehicles/3563ad5c-d92b-4152-ac57-c720bdbd9fb6/location',
+                headers: {
+                    Authorization: "bearer a3a1416d-53bc-4025-b760-e74129d35310"
+                }
+            }).then(resp => resp.data);
+
+            var odometer = $http({
+                method: 'GET',
+                url: 'https://api.smartcar.com/v1.0/vehicles/3563ad5c-d92b-4152-ac57-c720bdbd9fb6/odometer',
+                headers: {
+                    Authorization: "bearer a3a1416d-53bc-4025-b760-e74129d35310"
+                }
+            }).then(resp => resp.data);
+
+            return $q.all({ tires, fuel, location, odometer });
+        }
+    }
+})();
