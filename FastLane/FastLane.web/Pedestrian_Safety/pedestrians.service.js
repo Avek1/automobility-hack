9deb3b376b3db71c642042bd4407bbc10570359b
kinarki <nickfriend81@gmail.com>
2017-11-27 19:23:52 -0800

    pushing for pull from master

---
+++ FastLane/FastLane.web/Pedestrian_Safety/pedestrians.service.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+    angular
+        .module("fastLaneApp")
+        .factory("pedestrianService", PedestrianService);
+
+    PedestrianService.$inject = ["$http", "$q"];
+
+    function PedestrianService($http, $q) {
+        return {
+            authorize: _authorize
+        }
+
+        function _authorize() {
+            return $http.get('',
+                { withCredentials: true })
+                .then(_authorizeSuccess)
+                .catch(_authorizeError);
+        }
+
+        function _authorizeSuccess(res) {
+            return res;
+        }
+
+        function _authorizeError(err) {
+            return $q.reject(err);
+        }
+    }
+})();
